e114749ab6e2d1f2ea4199991ba6534a736c9edf
dreaife <877261793@qq.com>
2021-09-14 14:50:39 +0800

    java scanner trying

--- java/test/src/src/App.java
+++ java/test/src/src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        System.out.println(n);
         System.out.println("Hello, World!");
+        cin.close();
     }
 }
