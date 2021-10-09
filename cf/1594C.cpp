72864ff2ec2b1237c04c229cd97d56adf8da45d1
dreaife <877261793@qq.com>
2021-10-09 21:00:39 +0800

    common

--- cf/1594C.cpp
+++ cf/1594C.cpp
@@ -15,14 +15,9 @@ void solve(){
     for(int i=0;i<n;i++) if(s[i]==a)
         t++;
     if(t == n) {cout<<0<<endl;return ;}
-    if(s[n-1]==a&&s[n-2]==a) {cout<<1<<endl;cout<<n<<endl;return ;}
-    if(s[n-1]==a&&s[n-2]!=a) {cout<<1<<endl;cout<<n<<endl;return ;}
-    if(s[n-2]==a&&s[n-1]!=a) {cout<<1<<endl;cout<<n-1<<endl;return ;}
-    if(s[n-1]!=a&&s[n-2]!=a) {
-        for(int i=0;i<n;i++) if(s[i] == a && n%(i+1) && (n-1)%(i+1)){
-            cout<<1<<endl<<i+1<<endl;return;
-        }cout<<2<<endl;cout<<n-1<<' '<<n<<endl;
-    }
+    for(int i=n/2;i<n;i++) if(s[i]==a)
+        return cout<<1<<endl<<i+1<<endl,void();
+    cout<<2<<endl<<n-1<<' '<<n<<endl;
 }
 int main(){
     ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
