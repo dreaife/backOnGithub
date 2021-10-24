86960f44777f5f0032a60bd074c6ddd7d49a0f8c
dreaife <877261793@qq.com>
2021-10-24 19:20:42 +0800

    daily

---
+++ cf/15860.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+typedef long long ll;
+const int N=110;
+int n,q[N],n0,n1;
+//string s;
+void solve(){
+    cin>>n;n0=0,n1=0;
+    for(int i=0;i<n;i++) {cin>>a[i];if(a[i]%2) n1++;else n0++;}
+    
+}
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int _=0;cin>>_;
+    while(_--) solve();
+    return 0;
+}
