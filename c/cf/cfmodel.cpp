8b32bd0400b7fcef80591f62048c3d0e6cd36447
dreaife <877261793@qq.com>
2021-10-31 14:15:48 +0800

    update

--- c/cf/cfmodel.cpp
+++ c/cf/cfmodel.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
-#include<algorithm>
 #include<cstring>
+#include<algorithm>
 using namespace std;
 typedef long long ll;
-//const int N=1e5+10;
-//int q[N];
+//const int N = 1e5+10;
+//int a[N];
+//int n;
 //string s;
 void solve(){
 
 }
 int main(){
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    int _=0;cin>>_;
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int _;cin>>_;
     while(_--) solve();
     return 0;
 }
