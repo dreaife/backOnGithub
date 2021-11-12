577746a0c5bbb84cf5f7a33624d93503ef4e39af
dreaife <877261793@qq.com>
2021-11-12 17:11:26 +0800

    update

--- cf/15860.cpp
+++ cf/15860.cpp
@@ -1,25 +1,243 @@
 #include<iostream>
-#include<algorithm>
+#include<list>
 #include<cstring>
 using namespace std;
-typedef long long ll;
-//const int N=1e5+10;
-//int q[N];
-//string s;
-ll n,ans,n0;
-void solve(){
-    cin>>n;n0=0;ans=0;
-    for(int i=0,t;i<n;i++){
-        cin >> t;
-        if(!t) n0++;
-        if(t==1) ans++;
-    }
-    ans *= 1ll<<n0;
-    cout<<ans<<endl;
-}
+const int MAXLEN = 1e5+10;
+
+
+
+class Num{
+    class Node{
+	public:
+		int val;
+		Node*next;
+		Node(int v, Node*n)
+		{
+			val = v;
+			next = n;
+		}
+	};
+	Node*head;
+	int size;
+    bool D;
+    public:
+    Num(){
+		head = NULL;
+		size = 0;
+		D = false;
+	}
+    Num(string&s){
+		D = false;
+		head = NULL;
+		size = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] == ','||s[i]=='-')
+				continue;
+			size++;
+			if (s[i] == '.')
+				head = new Node(-100, head);
+			else
+				head = new Node(s[i] - '0', head);
+		}
+	}
+    Num(const Num& num)
+	{
+		D = num.D;
+		size = num.size;
+		head = NULL;
+		Node*cur = num.head, *temp=head;
+		while (cur)
+		{
+			if (!head)
+			{
+				head = new Node(cur->val, NULL);
+				temp = head;
+			}
+			else
+			{
+				temp->next = new Node(cur->val, NULL);
+				temp = temp->next;
+			}
+			cur = cur->next;
+		}
+	}
+	Num& operator=(const Num&num){
+		if (this == &num)
+			return *this;
+		free();
+		size = num.size;
+		head = NULL;
+		D = num.D;
+		Node*cur = num.head, *temp=head;
+		while (cur)
+		{
+			if (!head)
+			{
+				head = new Node(cur->val, NULL);
+				temp = head;
+			}
+			else
+			{
+				temp->next = new Node(cur->val, NULL);
+				temp = temp->next;
+			}
+			cur = cur->next;
+		}
+		return *this;
+	}
+    ~Num(){
+		free();
+	}
+	void free(){
+		size = 0;
+		while (head)
+		{
+			Node*temp = head->next;
+			delete head;
+			head = temp;
+		}
+		head = NULL;
+	}
+    Num& operator+=(const Num&num){
+		Node*pa = head, *pb = num.head;
+		while (pa||pb)
+		{
+			int b = 0;
+			if (pb)
+			{
+				b = pb->val;
+				pb = pb->next;
+			}
+			if (pa->val != -100)
+			{
+				pa->val += b;
+				if (pa->val > 9)
+				{
+					if (pa->next)
+					{
+						if (pa->next->val == -100)
+							pa->next->next->val++;
+						else
+							pa->next->val++;
+					}
+					else
+					{
+						pa->next = new Node(1, NULL);
+						size++;
+					}
+					pa->val -= 10;
+				}
+			}
+			pa = pa->next;
+		}
+		return *this;
+	}
+	Num& operator-=(const Num&num){
+		Node*pa = head, *pb = num.head;
+		while (pa || pb)
+		{
+			int b = 0;
+			if (pb)
+			{
+				b = pb->val;
+				pb = pb->next;
+			}
+			if (pa->val != -100)
+			{
+				pa->val -= b;
+				if (pa->val < 0)
+				{
+					if (pa->next->val == -100)
+						pa->next->next->val--;
+					else
+						pa->next->val--;
+					pa->val += 10;
+				}
+			}
+			pa = pa->next;
+		}
+		return *this;
+	}
+    friend ostream& operator<<(ostream&os, const Num&num){
+		bool z = false;
+		Num::Node* temp = num.head;
+		string s(num.size, '0');
+		for (int i = num.size - 1; i >= 0; --i)
+		{
+			if (temp->val == -100)
+			{
+				s[i] = '.';
+				z = true;
+			}
+			else
+				s[i] = temp->val + '0';
+			temp = temp->next;
+		}
+		if (num.D)
+		{
+			if (s.size() <= 11)
+				s = "0" + string(11 - s.size(), '0') + s;
+			if (s[s.size() - 1] > '4')
+				s[s.size() - 2]++;
+			s.erase(s.end() - 1);
+			for (int j = s.size() - 1; j > 0; --j)
+			{
+				if (s[j] > '9')
+				{
+					s[j - 1]++;
+					s[j] -= 10;
+				}
+				else
+					break;
+			}
+			if (s[0] > '9')
+			{
+				s[0] -= 10;
+				s = "1" + s;
+			}
+			s.insert(s.size() - 10, ".");
+			z = true;
+		}
+		int n = 0;
+		while (s[n] == '0')
+			n++;
+		if (n == s.size())
+			s = "0";
+		else
+			s = s.substr(n);
+		if (s[0] == '.')
+			s = '0' + s;
+		bool t = true;
+		int q;
+		for (q = 0; q < s.size(); ++q)
+			if (s[q] == '.')
+				break;
+		int j = s.size() - 1;
+		if (z)
+		{
+			while (j > q&&s[j] == '0')
+				j--;
+			if (j == q)
+				j--;
+		}
+		for (int i = 0; i <= j; ++i)
+		{
+			if (s[i] == '.')
+				t = false;
+			os << s[i];
+		}
+		os << endl;
+		return os;
+	}
+};
+
 int main(){
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    int _=0;cin>>_;
-    while(_--) solve();
+    string t1,t2;
+    cin>>t1>>t2;
+    Num a(t1),b(t2);
+    Num resAdd(a),resDiv(a);
+    resAdd+=b;resDiv-=b;
+    cout<<resAdd<<endl<<resDiv<<endl;
     return 0;
 }
--- cf/1606B.cpp
+++ cf/1606B.cpp
@@ -1,28 +1,112 @@
 #include<iostream>
-#include<algorithm>
+#include<list>
 #include<cstring>
 using namespace std;
-typedef long long ll;
-//const int N=1e5+10;
-//int q[N];
-//string s;
-ll n,k;
-void solve(){
-    cin>>n>>k;n--;
-    if(!n) return cout<<0<<endl,void();
-    ll t=1,cnt=1;n--;
-    while(n>0){
-        if(t*2ll < k) cnt++,t *= 2ll,n-=t;
-        else{
-            cnt += n/k;
-            if(n%k) cnt++;
-            break;
-        }
-    }cout<<max(0ll,cnt)<<endl;
+const int MAXLEN = 1e5+10;
+
+int get(list<int> t,int p){int i=0;
+    for(auto it = t.begin();;i++,it++) if(i==p)
+        return *it;
+    return 0;
 }
+
+struct BigInt{
+    const static int mod = 10000;
+    const static int DLEN = 4;
+    const static int MAXN = 9999;
+    list<int> a;
+    int len;
+    BigInt(){
+        a.clear();
+        len = 1;
+    }
+    BigInt(const char s[]){
+        a.clear();
+        int L = strlen(s);
+        len = L/DLEN;
+        if (L%DLEN) len++;
+        int index = 0;
+        for(int i=L-1;i>=0;i -= DLEN){
+            int t = 0;
+            int k=i-DLEN+1;
+            if(k<0) k = 0;
+            for(int j=k;j<=i;j++) t = t*10+s[j]-'0';
+            a.push_back(t);index++;
+        }
+    }
+    BigInt operator +(const BigInt &b)const{
+        BigInt res;
+        res.len = max(len,b.len);
+        res.a.clear();auto it = res.a.begin();
+        for(int i=0;i<res.len;i++,it++){
+            res.a.push_back(0);
+            *it += ((i<len)?get(a,i):0)+((i<b.len)?get(b.a,i):0);
+            *(++it) += *it/mod;
+            *(--it) %= mod;
+        }
+        return res;
+    }
+    bool operator >(const BigInt &b)const{
+        auto ln1 = a.end(),ln2=b.a.end();
+        if(len>b.len) return true;
+        else if(len == b.len){
+            while(*ln1 == *ln2 && ln1!=a.begin())
+                ln1--,ln2--;
+            if(ln1!=a.begin() && *ln1>*ln2)
+                return true;
+            else return false;
+        }else return false;
+    }
+    BigInt operator -(const BigInt &b)const{
+        int i,j,big;
+        BigInt t1,t2;
+        bool flag;
+        if(*this>b){
+            t1 = *this;
+            t2=b;
+            flag=0;
+        }
+        else {
+            t1=b;
+            t2=*this;
+            flag =1;
+        }
+        big = t1.len;i=0;
+        auto j1=t1.a.begin(),j2=t2.a.begin();
+        auto i1=t1.a.begin(),i2=t2.a.begin();
+        for(;i<big;i++,i1++,i2++){
+            if(*i1<*i2){
+                j1=++i1;i1--;j=i+1;
+                while(*j1==0) j++,j1++;
+                *(j1--)--;j--;
+                while(j>i) *(j1--)+=MAXN,j--;
+                *i1+=MAXN+1-*i2;
+            }
+            else *i1-=*i2;
+        }t1.len=big;
+        while(*(--i1)==0 && t1.len>1){
+            t1.len--;
+            big--;
+        }*i1++;
+        if(flag) *i1=0-*i1;
+        return t1;
+    }
+    void output(){
+        for(auto i=a.end();i!=a.begin();i--){
+            if(i!=++a.begin()) cout<<*i;
+            else cout<<*i-1;
+        }
+        cout<<endl;
+    }
+
+};
+
 int main(){
-    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    int _=0;cin>>_;
-    while(_--) solve();
+    char a[MAXLEN],b[MAXLEN];
+    cin>>a>>b;
+    BigInt aa(a),bb(b);
+    BigInt resAdd = aa+bb,resDiv = aa-bb;
+    resAdd.output();
+    resDiv.output();
     return 0;
 }
