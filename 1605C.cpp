ce3f5ff920ba969cc7773d944692868089443e91
dreaife <877261793@qq.com>
2021-11-13 01:08:08 +0800

    codeforce 1605C backSubmit

--- 1605C.cpp
+++ 1605C.cpp
@@ -6,17 +6,19 @@ using namespace std;
 typedef long long ll;
 const int N = 1e6+10;
 int n,a,b,c;
+vector<int> ans;
 string s;
 void solve(){
     cin>>n>>s;
-    a=0;b=0;c=0;
+    a=0;b=0;c=0;ans.clear();
     for(int i=0;i<n;i++){
         if(s[i]!='a') continue;
-        if(s.substr(i,2)=="aa") return cout<<2<<endl,void();
-        if(s.substr(i,3)=="aba"||s.substr(i,3)=="aca") return cout<<3<<endl,void();
-        if(s.substr(i,4)=="abca"||s.substr(i,4)=="acba") return cout<<4<<endl,void();
-        if(s.substr(i,7)=="abbacca"||s.substr(i,7)=="accabba") return cout<<7<<endl,void();
-    }cout<<-1<<endl;
+        if(s.substr(i,2)=="aa") ans.push_back(2);
+        if(s.substr(i,3)=="aba"||s.substr(i,3)=="aca") ans.push_back(3);
+        if(s.substr(i,4)=="abca"||s.substr(i,4)=="acba") ans.push_back(4);
+        if(s.substr(i,7)=="abbacca"||s.substr(i,7)=="accabba") ans.push_back(7);
+    }if(ans.empty()) cout<<-1<<endl;
+    else cout<<*min_element(ans.begin(),ans.end())<<endl;
 }
 int main(){
     ios::sync_with_stdio(false);
