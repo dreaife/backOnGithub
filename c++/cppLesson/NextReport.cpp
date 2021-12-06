850114aa5fbca551ff3c1303efff1016e928dfa2
dreaife <877261793@qq.com>
2021-12-07 01:30:36 +0800

    c++ report 6

--- c++/cppLesson/NextReport.cpp
+++ c++/cppLesson/NextReport.cpp
@@ -1,20 +1,36 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int cnt;
-void bubbleSort(int arr[],int x){
-	for(int i=0;i<x;i++)
-		for(int j=i+1;j<x;j++)
-			if(arr[i]>arr[j]) swap(arr[i],arr[j]),cnt++;
-}
-int main()
-{
-	int *a = new int[10]();cnt=0;
-	cout<<"输入a数组"<<endl;
-	for(int i=0;i<10;i++) cin>>a[i];
-	bubbleSort(a,10);
-	cout<<"a数组排序后结果:"<<endl;
-	for(int i=0;i<10;i++) cout<<a[i]<<' ';cout<<endl;
-	cout<<"冒泡排序交换次数:"<<cnt<<endl;
+class Point{
+	public:
+		Point(){
+			x = 0;y = 0;
+		}
+		Point(double a,double b):x(a),y(b){}
+		void move(double a,double b){
+			x = a;y = b;
+		}
+		~Point(){
+			x = 0;y = 0;
+		}
+		double x,y;
+};
+class Rectangle:public Point{
+	public:
+		Rectangle():Point(),w(0),h(0){}
+		Rectangle(double a,double b,double ww,double hh):Point(a,b),w(ww),h(hh){}
+		void getMess(){
+			cout<<"Left-Down Point:("<<x<<','<<y<<")\n";
+			cout<<"Weight:"<<w<<"\tHeight:"<<h<<endl;
+			cout<<"Area:"<<w*h<<endl;
+		}
+		~Rectangle(){
+			w = 0;h = 0;
+		}
+		double w,h;
+};
+int main(){
+	Rectangle rec(1,1,5,8);
+	rec.getMess();
 	return 0;
 }
