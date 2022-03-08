72f263f804ec22e722a0a9e00fb4fbfa760a9282
dreaife <877261793@qq.com>
2022-03-08 22:31:10 +0800

    codeforces

--- xjuEvent/a.cpp
+++ xjuEvent/a.cpp
@@ -1,19 +1,29 @@
 #include<iostream>
 #include<cstring>
+#include<cmath>
 #include<algorithm>
 using namespace std;
 typedef long long ll;
-//const int N = 1e5+10;
+const int N = 1010;
 //int a[N];
-//int n;
-//string s;
+int n,V,M;
+int v[N],m[N],w[N],f[N][N];
+// string s;
 void solve(){
-    
+    cin>>n>>V>>M;
+    for(int i=1;i<=n;i++) cin>>v[i]>>m[i]>>w[i];
+    for(int i=1;i<=n;i++){
+        for(int j=V;j>=v[i];j--){
+            for(int k=M;k>=m[i];k--){
+                f[j][k] = max(f[j][k],f[j-v[i]][k-m[i]]+w[i]);
+            }
+        }
+    }cout<<f[V][M]<<endl;
 }
-int main(){
+signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
-    int _;cin>>_;
+    int _=1;
     while(_--) solve();
     return 0;
 }
