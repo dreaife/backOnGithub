58de38c612fb7d0341c0302233a72e60de793c80
dreaife <877261793@qq.com>
2022-03-11 23:25:34 +0800

    xju test

--- xjuEvent/a.cpp
+++ xjuEvent/a.cpp
@@ -1,29 +1,26 @@
-#include<iostream>
-#include<cstring>
-#include<cmath>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const int N = 1010;
-//int a[N];
-int n,V,M;
-int v[N],m[N],w[N],f[N][N];
-// string s;
-void solve(){
-    cin>>n>>V>>M;
-    for(int i=1;i<=n;i++) cin>>v[i]>>m[i]>>w[i];
-    for(int i=1;i<=n;i++){
-        for(int j=V;j>=v[i];j--){
-            for(int k=M;k>=m[i];k--){
-                f[j][k] = max(f[j][k],f[j-v[i]][k-m[i]]+w[i]);
-            }
-        }
-    }cout<<f[V][M]<<endl;
-}
-signed main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    int _=1;
-    while(_--) solve();
-    return 0;
+#define int long long 
+const int N=2e5+10; 
+int h[N];
+map<int,int>mp;
+signed main()
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+		cin>>h[i];
+	int sum=0;
+	for(int i=1;i<=n;i++)
+	{
+		int j=i-h[i];
+		sum += mp[j];
+
+        j=h[i];
+		sum += mp[j];
+		
+		mp[h[i]+i]++;
+	}
+	cout << sum<<endl;
+	return 0;
 }
