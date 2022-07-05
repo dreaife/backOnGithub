0de91fefe4deafda3d70f2c1803618d57c5ad74f
dreaife <877261793@qq.com>
2022-07-05 21:08:18 +0800

    middleSummerDay02

---
+++ java/idea workspace/tempMiddleTest/src/summerMiddle/Homework2/Books.java
@@ -0,0 +1,13 @@
+package summerMiddle.Homework2;
+
+public class Books {
+    int value;
+
+    public Books(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ java/idea workspace/tempMiddleTest/src/summerMiddle/Homework2/homeworkTwo.java
@@ -0,0 +1,35 @@
+package summerMiddle.Homework2;
+
+import java.util.*;
+
+public class homeworkTwo {
+    public static void main(String[] args) {
+        TreeSet<Students> stu = new TreeSet<>(new Comparator<Students>() {
+            @Override
+            public int compare(Students o1, Students o2) {
+                return (o1.getValue() - o2.getValue());
+            }
+        });
+        stu.add(new Students("12345","ww",1,18,new ArrayList<>(){{
+            add(new Books(1));add(new Books(3));add(new Books(2));
+        }}));
+        stu.add(new Students("12346","wn",0,19,new ArrayList<>(){{
+            add(new Books(4));add(new Books(1));add(new Books(2));
+        }}));
+        stu.add(new Students("12347","qq",1,22,new ArrayList<>(){{
+            add(new Books(1));add(new Books(2));
+        }}));
+        stu.add(new Students("12348","bt",0,17,new ArrayList<>(){{
+            add(new Books(1));add(new Books(3));
+        }}));
+        stu.add(new Students("12349","bw",1,20,new ArrayList<>(){{
+            add(new Books(1));add(new Books(3));add(new Books(2));
+            add(new Books(4));
+        }}));
+
+        System.out.println("按照借阅书本价值升序对学生排序：");
+        for (Students students : stu) {
+            System.out.println(students.toString());
+        }
+    }
+}
