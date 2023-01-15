cf4d9cabda38a7d5ef7bfde3fc8086cf67e29e75
dreaife <877261793@qq.com>
2023-01-16 01:01:06 +0800

    csapp chapter01

---
+++ cf/1783A.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+#define endl '\n'
+#define all(v) v.begin(),v.end()
+typedef long long ll;
+typedef pair<int,int> PII;
+
+const int N = 2e5+10;
+int a[N];
+int n;
+//string s;
+void solve(){
+    cin>>n;
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a,a+n,greater<int>());
+    if(a[0] == a[n-1]) return void(cout<<"NO"<<endl);
+    cout<<"YES"<<endl;
+    cout<<a[0]<<' '<<a[n-1];
+    for(int i=1;i<n-1;i++) cout<<' '<<a[i];
+    cout<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int _;cin>>_;
+    while(_--) solve();
+    return 0;
+}
